81fa9f2fced4d7b62b406c95200f2cbc783c7d9b
Reza Jaber <frontendrezajaber@gmail.com>
2024-04-21 18:22:18 +0200

    create layout for app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,12 @@
 	import TaskCreation from '../lib/components/TaskCreation.svelte';
 </script>
 
-<div class="text-center">
-	<TaskOverview />
-	<TaskCreation />
+<div class="grid gap-4 md:flex md:gap-6">
+	<div class="h-fit w-full md:h-auto lg:w-fit">
+		<TaskOverview />
+	</div>
+
+	<div class="w-full md:h-auto lg:w-[32rem] xl:w-[40rem]">
+		<TaskCreation />
+	</div>
 </div>
