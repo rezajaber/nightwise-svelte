cb96e277f8590b99b9dff1bdb5e9b764ff741cdb
Reza Jaber <frontendrezajaber@gmail.com>
2024-04-21 18:30:48 +0200

    create Input for TaskCreation.svelte

--- src/lib/components/TaskCreation.svelte
+++ src/lib/components/TaskCreation.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { Input } from '$lib/components/ui/input';
 	import ControlBar from '$lib/components/taskcreation/ControlBar.svelte';
 	import TaskSideInformation from '$lib/components/taskcreation/TaskSideInformation.svelte';
 </script>
@@ -7,4 +8,7 @@
 	class="relative rounded-xl bg-gradient-to-b from-border to-card p-px px-4 py-5 shadow-lg transition-all"
 >
 	<ControlBar />
+	<Input
+		class="font-regular mt-7 w-full border-0 bg-secondary px-4 text-3xl text-primary caret-accent focus-visible:outline-none focus-visible:ring-0"
+	/>
 </div>
