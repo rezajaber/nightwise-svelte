25a88b727301a2d44aec82f8fb99e997250dcbdd
Reza Jaber <frontendrezajaber@gmail.com>
2024-04-21 18:14:04 +0200

    add card-background for TaskCreation

--- src/lib/components/TaskCreation.svelte
+++ src/lib/components/TaskCreation.svelte
@@ -3,6 +3,8 @@
 	import TaskSideInformation from '$lib/components/taskcreation/TaskSideInformation.svelte';
 </script>
 
-<div>
+<div
+	class="relative rounded-xl bg-gradient-to-b from-border to-card p-px px-4 py-5 shadow-lg transition-all"
+>
 	<ControlBar />
 </div>
